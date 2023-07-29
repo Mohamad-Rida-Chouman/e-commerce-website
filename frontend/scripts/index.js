101bf46ebbe2fe41b8ba6e3ae852508d970376b6
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-07-29 18:36:56 +0300

    added blur effect to index.html

---
+++ frontend/scripts/index.js
@@ -0,0 +1,19 @@
+const leftContainer = document.querySelector('.left-container');
+const rightContainer = document.querySelector('.right-container');
+
+leftContainer.addEventListener('mouseenter', () => {
+  leftContainer.classList.add('expand-background');
+});
+
+leftContainer.addEventListener('mouseleave', () => {
+  leftContainer.classList.remove('expand-background');
+});
+
+rightContainer.addEventListener('mouseenter', () => {
+  rightContainer.classList.add('expand-background');
+});
+
+rightContainer.addEventListener('mouseleave', () => {
+  rightContainer.classList.remove('expand-background');
+});
+
