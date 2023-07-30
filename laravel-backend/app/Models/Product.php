1b050120f896be3107247875e0964b241c2516f8
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-07-30 19:32:52 +0300

    added product apis

--- laravel-backend/app/Models/Product.php
+++ laravel-backend/app/Models/Product.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     protected $fillable = [
-        'name', 'description', 'category_id', 'price', 'image',
+        'name', 'description', 'price', 'image',
     ];
 
     public function categories() {
