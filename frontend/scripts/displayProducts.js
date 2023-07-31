f3b45b52327b597ccb92e5f79141497e5f5db253
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-01 01:10:42 +0300

    fixed fav/cart buttons on login/logout

--- frontend/scripts/displayProducts.js
+++ frontend/scripts/displayProducts.js
@@ -23,6 +23,24 @@ window.addEventListener('load', async () => {
 		DISPLAY_URL = PROD_BASE_URL;
 	}
 
+	const token = localStorage.getItem('token');
+	const buttonChange = document.getElementById('login')
+	if(token){
+		buttonChange.innerHTML = `logout`;
+		buttonChange.addEventListener('click', () => {
+			if(logout()){
+				console.log("logged out")
+				window.location.reload();
+
+			}
+		})
+	}
+	else{
+		buttonChange.addEventListener('click', () => {
+		window.location.href = `userLogin.html`;
+	});
+	}
+
 	const productsContainer = document.getElementById('main-container');
 	if (productsContainer) {
 		const products = await getProducts();
@@ -43,20 +61,56 @@ window.addEventListener('load', async () => {
 				product.description +
 				'</div><div class="product-price">' +
 				product.price +
-				'</div> <a href="#" class="fav-btn" id="'+product.id+'">Add to favorites</a> <a href="#" class="cart-btn" id="cart-btn">Add to cart</a></div>';
+				'</div> <a href="#" class="fav-btn" id="fav'+product.id+'">Add to favorites</a> <a href="#" class="cart-btn" id="cart'+product.id+'">Add to cart</a></div>';
 		}
 
 		productsContainer.innerHTML = productsList;
 
-		const favBtn = document.querySelectorAll(".fav-btn")
-		// favBtn.addEventListener("click",()=>{
-		// 	console.log(favBtn.id)
-		// })
+		const token = localStorage.getItem('token');
+		if(!token){
+			const favBtn = document.querySelectorAll(".fav-btn");
+			favBtn.forEach(function(btnFav) {
+				document.getElementById(btnFav.id).style.display = "none";
+			});
+			const cartBtn = document.querySelectorAll(".cart-btn");
+			
+			cartBtn.forEach(function(btnCart) {
+				console.log(btnCart.id)
+				document.getElementById(btnCart.id).style.display = "none";
+			});
+		}
 
+		const favBtn = document.querySelectorAll(".fav-btn");
 		favBtn.forEach(function(btn) {
-			btn.addEventListener('click', ()=> {
-				console.log(btn.id)
+			btn.addEventListener('click', async()=> {
+				$id = btn.id.replace('fav','')
+				console.log($id)
+				const FAV_URL = PROD_BASE_URL + '/' + $id + '/usersFav'
+
+				const responseFav = await axios.post(`${FAV_URL}`, {
+					user_id: localStorage.getItem('token'),
+				});
+			});
+		  });
+		const cartBtn = document.querySelectorAll(".cart-btn");
+		cartBtn.forEach(function(btn) {
+			btn.addEventListener('click', async()=> {
+				$id = btn.id.replace('cart','')
+				console.log($id)
+				const CART_URL = PROD_BASE_URL + '/' + $id + '/usersCart'
+
+				const responseCart = await axios.post(`${CART_URL}`, {
+					user_id: localStorage.getItem('token'),
+				});
 			});
 		  });
 	}
 });
+const logout = async () => {
+	try {
+		localStorage.removeItem('token');
+		return true;
+	} catch (errors) {
+		console.error(errors);
+	}
+};
